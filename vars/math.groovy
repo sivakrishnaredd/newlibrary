48e260007c6a519cf3f5d28f6bb71d02870a81c1
sivakrishnaredd <140617664+sivakrishnaredd@users.noreply.github.com>
2023-09-12 18:41:44 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,1 +1,15 @@
-
+def sum(a,b)
+{
+  echo the sum of ${a} and &{b} is ${a+b}"
+}
+def sub(a,b)
+{
+  echo the subtraction resultof of ${a} and ${b} of ${a-b}"
+}
+def mul(a,b)
+{
+  echo the product of ${a} and ${b} of ${a*b}"
+}
+def div(a,b)
+{
+  echo the devition of ${a} and ${b} of ${a/b}"
